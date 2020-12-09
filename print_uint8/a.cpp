ffc018c700d6364447ffb5b43fa251f309f5aedf
cosmicog <orhangazi4401@gmail.com>
2020-12-09 04:11:13 +0300

    Add uint8 / boolean example

---
+++ print_uint8/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main ()
+{
+    uint8_t a = false;
+    uint8_t b = true;
+    
+    cout << unsigned(a) << " " << unsigned(b) << endl;
+return 0;
+}
